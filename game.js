b626bd3ee5afc8fb29ff2eab2a9da4f4789f9958
Ben Williamson <58665591+BenWilliamson52@users.noreply.github.com>
2021-03-23 10:50:09 +0000

    Add files via upload
    basic mechanics

---
+++ game.js
@@ -0,0 +1,122 @@
+// get a handle to the canvas context
+var canvas = document.getElementById("canvas");
+// get 2D context for this canvas
+var context = canvas.getContext("2d");
+
+
+
+var enemyHealth = 3;
+var playerHealth = 3;
+
+function drawHealthbarEnemy() {
+  var width = 80;
+  var height = 8;
+  var max = 3;
+  var val = enemyHealth;
+
+  //Background
+  context.fillStyle = "#000000";
+  context.clearRect(0, 0, width, height);
+  context.fillRect(110, 20, width, height);
+
+  //Fill
+  context.fillStyle = "#00FF00";
+  var fillVal = Math.min(Math.max(val / max, 0), 1);
+  context.fillRect(110, 20, fillVal * width, height);
+}
+
+function drawHealthbarPlayer() {
+    var width = 80;
+    var height = 8;
+    var max = 3;
+    var val = playerHealth;
+  
+    //Background
+    context.fillStyle = "#000000";
+    context.clearRect(0, 0, width, height);
+    context.fillRect(130, 100, width, height);
+  
+    //Fill
+    context.fillStyle = "#00FF00";
+    var fillVal = Math.min(Math.max(val / max, 0), 1);
+    context.fillRect(130, 100, fillVal * width, height);
+  }
+
+
+
+// player image
+var sprite = new Image();
+sprite.src = "./back.png"; 
+// enemy image
+var enSprite = new Image();
+enSprite.src = "./front.png";
+
+
+// Updates
+function update() {
+    console.log("Update ...");
+}
+function draw() {
+    console.log("Draw");
+   animate();
+}
+    // Draw player
+function animate() {
+    context.drawImage(enSprite, 200,20,60,60)
+    context.drawImage(sprite, 45, 65, 80, 80);
+    drawHealthbarEnemy();
+    drawHealthbarPlayer();
+}
+
+
+
+var move = 1;
+
+// attacks
+function punchAttack(){
+    move = 1;
+    enemyAttack();
+}
+function biteAttack(){
+    move = 2;
+    enemyAttack();
+}
+function counterAttack(){
+    move = 3;
+    enemyAttack();
+ }
+
+ 
+
+// attacking
+function enemyAttack(){
+
+    var random = Math.floor(Math.random() * 3) + 1;
+    
+    if (random == 1 && move == 1){playerHealth -= 0}
+    if (random == 1 && move == 2){playerHealth -= 1}
+    if (random == 1 && move == 3){enemyHealth -= 1}
+
+    if(random == 2 && move == 1){enemyHealth -= 1}
+    if(random == 2 && move == 2){playerHealth -= 0}
+    if(random == 2 && move == 3){playerHealth -= 1}
+
+    if(random == 3 && move == 1){playerHealth -= 1}
+    if(random == 3 && move == 2){enemyHealth -= 1}
+    if(random == 3 && move == 3){playerHealth -=0}
+}
+
+
+
+function gameloop() {
+    
+    update();
+    draw();
+    window.requestAnimationFrame(gameloop);
+}
+
+
+window.requestAnimationFrame(gameloop);
+
+
+
